aed427d1de148d92ba04ee2460d352c3c66c5b0d
FlowTech <admin@FlowTech-LAB.org>
2025-10-06 20:12:13 +0200

    Enhance theme support and improve contact and team pages layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "pages/team"
-  get "/contact", to: 'pages#contact'
-  get "up" => "rails/health#show", as: :rails_health_check
+  root "pages#home"
 
+  get "pages/team"
+  get "/contact", to: "pages#contact"
+  get "up" => "rails/health#show", as: :rails_health_check
 end
