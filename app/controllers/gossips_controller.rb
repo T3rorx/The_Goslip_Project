13a422ed94a515abb1d84c9b90f3d59ebfd0186c
FlowTech <admin@FlowTech-LAB.org>
2025-10-07 17:21:16 +0200

    Refactor gossip functionality: remove GossipController and related views, implement GossipsController with actions for show, new, and create, and update routes accordingly.

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,25 @@
+class GossipsController < ApplicationController
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create
+    @gossip = Gossip.new(gossip_params)
+
+    if @gossip.save
+      redirect_to root_path, notice: "Le potin a été créé avec succès."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def gossip_params
+    params.require(:gossip).permit(:title, :content, :user_id)
+  end
+end
