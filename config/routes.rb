619d2f639a9baf1ca8464aec2eaf253ace761a68
Ton nom d'utilisateur github <enzonatali75@gmail.com>
2025-10-08 16:24:06 +0200

    Ajout des contrôleurs Cities et Comments, mise à jour des routes et des vues associées pour gérer les villes et les commentaires.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get "pages/team"
   get "/contact", to: "pages#contact"
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :cities
+  resources :comments
 end
